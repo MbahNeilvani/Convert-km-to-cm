52eeb4485306d3de50b66baf37e9a999c5aa2aad
Mbah Neilvani <neilvanimbah@gmail.com>
2024-03-28 08:13:03 +0100

    Converting speed in km/h to cm/s

---
+++ Js/convert.js
@@ -0,0 +1,10 @@
+// The cockroach is one of the fastest insects. Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer (= floored)
+
+function cockroachSpeed(s) {
+    //Good Luck!
+    let speedInKm = 0
+    let finalSpeed = Math.round( speedInKm *  27.778)
+    console.log( finalSpeed )
+  }
+  
+  cockroachSpeed(25)
