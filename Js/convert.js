ccc32774a0847e1abdc5dffd7ee7244f50509aad
Mbah Neilvani <neilvanimbah@gmail.com>
2024-03-28 08:33:54 +0100

    made the code to be correct

--- Js/convert.js
+++ Js/convert.js
@@ -8,3 +8,31 @@ function cockroachSpeed(s) {
   }
   
   cockroachSpeed(25)
+
+  //corrections
+
+  function cockroachSpeed(s) {
+    //multiply km/m by 27.7777777778
+    return Math.floor(s*27.7777777778);
+  }
+ // next 
+
+  const cockroachSpeed = s => Math.floor(s / 0.036);
+  
+  // next
+
+  function cockroachSpeed(s) {
+    const KILOMETER_IN_CENTIMETERS = 1 * 1000 * 100;
+    const HOUR_IN_SECONDS = 1 * 60 * 60;
+    
+    return Math.floor(s * KILOMETER_IN_CENTIMETERS / HOUR_IN_SECONDS);
+  }
+ 
+  //Mine re-taken
+
+  function cockroachSpeed(s){
+      let sleepInKmToCm = 1 * 1000 *100;
+      let speedInHtoS = 1 * 60 * 60;
+      
+      return Math.round(s * sleepInKmToCm / speedInHtoS);
+  }
